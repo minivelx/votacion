975b0a0a3f2a9e42fd307e7ace531462e65cad63
muribe <minivelx@gmail.com>
2020-02-03 20:18:58 -0500

    avance 5

--- Controllers/CandidatosController.cs
+++ Controllers/CandidatosController.cs
@@ -31,5 +31,27 @@ namespace WebApiVotacion.Controllers
             }
         }
 
+        [HttpGet("crear")]
+        public async Task<IActionResult> CrearCandidatos()
+        {
+            try
+            {
+                var candidat1 = new Candidato { Id = 0, Nombre = "JUAN FERNANDO RESTREPO", FotoBase64 = null };
+                var candidat2 = new Candidato { Id = 0, Nombre = "JUAN FELIPE MOSQUERA", FotoBase64 = null };
+                var candidat3 = new Candidato { Id = 0, Nombre = "JUAN LUIS URIBE", FotoBase64 = null };
+                _context.Candidatos.Add(candidat1);
+                _context.Candidatos.Add(candidat2);
+                _context.Candidatos.Add(candidat3);
+                await _context.SaveChangesAsync();
+
+                return Json(new { success = true, message = "Registros guardados." });
+            }
+            catch (Exception exc)
+            {
+                string ErrorMsg = exc.GetBaseException().InnerException != null ? exc.GetBaseException().InnerException.Message : exc.GetBaseException().Message;
+                return Json(new { success = false, message = "Error!. " + ErrorMsg });
+            }
+        }
+
     }
 }
